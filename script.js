65825866cd04fd6952eaa7538108ff2f9066ddcc
YTseen <justsignmein1@gmail.com>
2025-04-03 15:11:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let selectedKey = "";
 
 async function loadQuests() {
   try {
-    const res = await fetch("./data/quest_data.json");
+    const res = await fetch("https://raw.githubusercontent.com/YTseen/bloodbot-campaign/main/data/quest_data.json");
     questData = await res.json();
     const questList = document.getElementById("questList");
     questList.innerHTML = "";
